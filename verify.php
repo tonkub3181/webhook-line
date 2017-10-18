90fcf725ad32153f1389ae1c45685702bb8b0709
tonkub3181 <32896484+tonkub3181@users.noreply.github.com>
2017-10-18 17:19:35 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'r21KrzNH0w2z6A6WxqvjXD97vN/7PRG3QfEqpvcNYyHFUZpSumySRKHCCHpkUlErWs9JNHL6EkevUuWVXGdY3yDWrXIl92k99Kx51QDtkwD1TgDRZUuag58GI9WdQ+kRA+Lj9YEYTnCTvLiW0X6jYgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
